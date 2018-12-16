eebb91c3ac2a760d05cdd4e553109928aa566f30
ramya <ramya.mounami@gmail.com>
2018-12-16 14:14:41 -0500

    Task: Added login with google link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ url('/redirect') }}">Login with Google</a>
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
